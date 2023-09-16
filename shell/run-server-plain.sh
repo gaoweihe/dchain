8c49cfdb990c7a403a231adfe0cb7f093f4323ac
GAO Weihe <gao.weihe@outlook.com>
2023-09-16 17:34:03 +0000

    update test config

--- shell/run-server-plain.sh
+++ shell/run-server-plain.sh
@@ -3,6 +3,6 @@ eval node_label=\$\($cmd\)
 
 mkdir -p ../log
 ../build/tc-server \
-    --cf ../conf/server-debug/server$node_label.json \
+    --cf ../conf/server-debug/server.json \
     --id $node_label \
     >../log/server-$node_label.out 2>&1 &
