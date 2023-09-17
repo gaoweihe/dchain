1e44a3edfcde8504ca8ea966780d9feb894a773c
GAO Weihe <gao.weihe@outlook.com>
2023-09-17 14:14:20 +0000

    modify assertions

--- include/server/tc-server-peer-grpc.hpp
+++ include/server/tc-server-peer-grpc.hpp
@@ -79,7 +79,7 @@ namespace tomchain
                 }
 
                 std::shared_ptr<tomchain::Block> block_sp = pb_accessor->second; 
-                assert(block_sp->votes_ != nullptr); 
+                assert(block_sp != nullptr); 
                 block_sp->votes_.insert(
                     std::make_pair(
                         vote->voter_id_,
