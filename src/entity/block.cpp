ed4eb3427b97a3e96b9ef253b85d4ae9cf317706
GAO Weihe <gao.weihe@outlook.com>
2023-09-19 18:36:41 +0000

    stage

--- src/entity/block.cpp
+++ src/entity/block.cpp
@@ -93,7 +93,7 @@ namespace tomchain
             // merge signature
             // size_t merge_threads = (*::conf_data)["merge-threads"]; 
             spdlog::trace("{}: merge sigset", target_num); 
-            std::shared_ptr<BLSSignature> tss_sig = sig_share_set.merge(1);
+            std::shared_ptr<BLSSignature> tss_sig = sig_share_set.merge(4);
             spdlog::trace("{}: merge complete", target_num); 
             this->tss_sig_ = tss_sig;
         }
