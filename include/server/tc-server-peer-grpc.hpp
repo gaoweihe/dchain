5c76cec63a72111d94e084b7793e2d7b79408735
GAO Weihe <gao.weihe@outlook.com>
2023-09-19 16:38:35 +0000

    stage

--- include/server/tc-server-peer-grpc.hpp
+++ include/server/tc-server-peer-grpc.hpp
@@ -440,44 +440,44 @@ namespace tomchain
 
         std::shared_ptr<Block> block;
         spdlog::trace("{} gRPC(SPBcastCommit) pops blocks", target_server_id);
-        while (bcast_commit_blocks.find(target_server_id)->second->try_pop(block))
-        {
-            // serialize block
-            msgpack::sbuffer b;
-            msgpack::pack(b, block);
-            std::string ser_block = sbufferToString(b);
+        // while (bcast_commit_blocks.find(target_server_id)->second->try_pop(block))
+        // {
+        //     // serialize block
+        //     msgpack::sbuffer b;
+        //     msgpack::pack(b, block);
+        //     std::string ser_block = sbufferToString(b);
 
-            // add to bcast block vector
-            request.add_blocks(ser_block);
-        }
+        //     // add to bcast block vector
+        //     request.add_blocks(ser_block);
+        // }
 
-        // if no commits, return 
-        if (request.blocks_size() == 0)
-        {
-            return grpc::Status::OK; 
-        } 
+        // // if no commits, return 
+        // if (request.blocks_size() == 0)
+        // {
+        //     return grpc::Status::OK; 
+        // } 
 
-        SPBcastCommitResponse response;
+        // SPBcastCommitResponse response;
 
-        grpc::ClientContext context;
-        std::mutex mu;
-        std::condition_variable cv;
-        bool done = false;
+        // grpc::ClientContext context;
+        // std::mutex mu;
+        // std::condition_variable cv;
+        // bool done = false;
 
-        spdlog::trace("{} gRPC(SPBcastCommit) waiting", target_server_id);
+        // spdlog::trace("{} gRPC(SPBcastCommit) waiting", target_server_id);
         grpc::Status status;
-        grpc_peer_client_stub_.find(target_server_id)->second->async()->SPBcastCommit(&context, &request, &response, [&mu, &cv, &done, &status](grpc::Status s)
-                                                                                      {
-                status = std::move(s);
-                std::lock_guard<std::mutex> lock(mu);
-                done = true;
-                cv.notify_one(); });
+        // grpc_peer_client_stub_.find(target_server_id)->second->async()->SPBcastCommit(&context, &request, &response, [&mu, &cv, &done, &status](grpc::Status s)
+        //                                                                               {
+        //         status = std::move(s);
+        //         std::lock_guard<std::mutex> lock(mu);
+        //         done = true;
+        //         cv.notify_one(); });
 
-        std::unique_lock<std::mutex> lock(mu);
-        while (!done)
-        {
-            cv.wait(lock);
-        }
+        // std::unique_lock<std::mutex> lock(mu);
+        // while (!done)
+        // {
+        //     cv.wait(lock);
+        // }
 
         spdlog::trace("gRPC(SPBcastCommit): {}:{}",
                       status.error_code(),
